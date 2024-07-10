8b57c8cdecf495b1bd1519f5f566ca4a9a827860
Aakash Kalmady <arkalmady@gmail.com>
2024-07-10 16:57:39 -0400

    adding navbar, js, lot of animation features

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 let hamMenu = document.getElementById("ham-menu");
 let menu = document.getElementById("menu");
 let sidebar = document.getElementById("sidebar");
+let navbar = document.getElementById("navbar");
 
 function toggleMenu() {
   menu.classList.toggle("menu-toggled");
   sidebar.classList.toggle("sidebar-toggled");
-  console.log("menu toggled");
+  hamMenu.classList.toggle("ham-menu-toggled");
 }
-menu.classList.toggle("menu-toggled");
+
 hamMenu.addEventListener("click", toggleMenu);
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > window.innerHeight - 65) {
+    navbar.className = "navbar-container-scrolled";
+  } else {
+    navbar.className = "navbar-container";
+  }
+});
