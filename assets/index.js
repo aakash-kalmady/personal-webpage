b0527b20ebef86367bd62ff8d9ac27bf70d79f0c
Aakash Kalmady <arkalmady@gmail.com>
2024-08-11 20:15:06 -0400

    new home page

--- assets/index.js
+++ assets/index.js
@@ -25,7 +25,7 @@ hamMenu.addEventListener("click", toggleMenu);
 //function to shift nav bar depending on how far one has scrolled and parallax for background
 window.addEventListener("scroll", () => {
   var height;
-  //background.style.top = -window.scrollY * 0.5 + "px";
+  //background.style.top = -window.scrollY * 0.15 + "px";
   if (window.innerWidth < 625) {
     height = homeMenu.offsetHeight - 75;
   } else {
