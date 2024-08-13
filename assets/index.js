a58d325c3dfff9079993c6b79c24deb85b605505
Aakash Kalmady <arkalmady@gmail.com>
2024-08-12 20:47:10 -0400

    changes to homepage socials and dock

--- assets/index.js
+++ assets/index.js
@@ -2,11 +2,8 @@ let hamMenu = document.getElementById("ham-menu");
 let menu = document.getElementById("menu-container");
 let sidebar = document.getElementById("sidebar");
 let navbar = document.getElementById("navbar-container");
-let scrollSection = document.getElementById("scroll-icon-container");
-let signature = document.getElementById("signature-container");
 let page = document.getElementById("main");
 let background = document.getElementById("background");
-let scrollWheel = document.getElementById("scroll-wheel");
 let homeMenu = document.getElementById("home");
 
 //section elements
@@ -25,7 +22,7 @@ hamMenu.addEventListener("click", toggleMenu);
 //function to shift nav bar depending on how far one has scrolled and parallax for background
 window.addEventListener("scroll", () => {
   var height;
-  //background.style.top = -window.scrollY * 0.15 + "px";
+  background.style.top = -window.scrollY * 0.1 + "px";
   if (window.innerWidth < 625) {
     height = homeMenu.offsetHeight - 75;
   } else {
@@ -34,12 +31,8 @@ window.addEventListener("scroll", () => {
   console.log(height);
   if (window.scrollY > height) {
     navbar.className = "navbar-container-scrolled";
-    signature.className = "signature-container-scrolled";
-    scrollSection.className = "scroll-icon-container-scrolled";
   } else {
     navbar.className = "navbar-container";
-    signature.className = "signature-container";
-    scrollSection.className = "scroll-icon-container";
   }
 });
 
