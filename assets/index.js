a8bd519532b4a963070dc56f80f7b69ca71f2221
Aakash Kalmady <arkalmady@gmail.com>
2024-09-08 22:48:37 -0400

    revealing elements on scroll

--- assets/index.js
+++ assets/index.js
@@ -14,29 +14,6 @@ let sidebar = document.getElementById("sidebar");
 let navbar = document.getElementById("navbar-container");
 let contentBlocker = document.getElementById("content-blocker");
 
-// click to toggle hamburger menu
-hamMenu.addEventListener("click", () => {
-  menu.classList.toggle("menu-container-toggled");
-  hamMenu.classList.toggle("ham-menu-toggled");
-});
-
-// stick navbar to top of page once scrolled, and parallax for background
-window.addEventListener("scroll", () => {
-  var height,
-    parallax = window.scrollY;
-  background.style.top = -parallax * 0.15 + "px";
-  if (window.innerWidth < 625) {
-    height = homeMenu.offsetHeight - 75;
-  } else {
-    height = window.innerHeight - 80;
-  }
-  if (window.scrollY > height) {
-    navbar.className = "navbar-container-scrolled";
-  } else {
-    navbar.className = "navbar-container";
-  }
-});
-
 // scroll to section functions
 function scrollToHome() {
   window.scroll({
@@ -55,6 +32,51 @@ function scrollToGallery() {
 function scrollToContact() {
   contact.scrollIntoView();
 }
+
+// loading screen
 function removeBlocker() {
   contentBlocker.style.opacity = 0;
 }
+
+// click to toggle hamburger menu
+hamMenu.addEventListener("click", () => {
+  menu.classList.toggle("menu-container-toggled");
+  hamMenu.classList.toggle("ham-menu-toggled");
+});
+
+// stick navbar to top of window if scrolled down
+function navbarController() {
+  var height,
+    parallax = window.scrollY;
+  background.style.top = -parallax * 0.15 + "px";
+  if (window.innerWidth < 625) {
+    height = homeMenu.offsetHeight - 75;
+  } else {
+    height = window.innerHeight - 80;
+  }
+  if (window.scrollY > height) {
+    navbar.className = "navbar-container-scrolled";
+  } else {
+    navbar.className = "navbar-container";
+  }
+}
+
+// let elements appear as they scroll
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 200;
+
+    if (revealtop < windowHeight - revealpoint) {
+      reveals[i].classList.add("active-reveal");
+    } else {
+      reveals[i].classList.remove("active-reveal");
+    }
+  }
+}
+
+window.addEventListener("scroll", navbarController, reveal);
+window.addEventListener("scroll", reveal);
